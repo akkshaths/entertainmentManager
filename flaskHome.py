da63bbde773cf64ccaafeb5829818db662c782f1
Aarushi Jain <aashijn11@gmail.com>
2020-08-22 21:29:48 -0400

    Update flaskHome.py

--- flaskHome.py
+++ flaskHome.py
@@ -15,14 +15,14 @@ def login():
     return render_template('signup.html')
     
 
-@app.route('/price')
+@app.route('/price', methods = ['POST'])
 def findPrice():
-    listOfSubTV = request.form['tv']
+    listOfSubTV = request.form.getlist('tv')
     forHtml = ''
     for i in listOfSubTV:
         forHtml += '''
-        <input type="text" name="''' + i + '''" value="1">How much do you pay for ''' + i + ''' per month?</input>
-        \n <input type="text" name="''' + i + '''" value="1">How many hours have you used this in the past week</input> \n'''
+        <input type="text" name="''' + i.tv + '''" value="1">How much do you pay for ''' + i.tv + ''' per month?</input>
+        \n <input type="text" name="''' + i.tv + '''" value="1">How many hours have you used this in the past week</input> \n'''
 
     return render_template('pay&hours.html', userInput = forHtml)
 
