b2ccec3450f1a86fcdd4e43fd38b1e51a4955218
Aarushi Jain <aashijn11@gmail.com>
2020-08-23 11:55:41 -0400

    Update users.py

--- users.py
+++ users.py
@@ -2,9 +2,9 @@ import sqlite3
 
 class newUser():
     def __init__ (self, userName, password):
-        sqliteConnection = sqlite3.connect('subs.db')
+        database = '/Users/vjain/subs.db'
+        sqliteConnection = sqlite3.connect(database)
         cursor = sqliteConnection.cursor()
-        # subs2.db = '/Users/Vinathi/Downloads/subs2.db'
 
         cursor.execute("INSERT INTO user (userID, userName, password) VALUES (1, userName, password)")
         print("Successfully added new user:", userName)
