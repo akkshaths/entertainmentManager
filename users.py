db1874862b20a9f7f3f3ae7071079e52b560f190
Aarushi Jain <aashijn11@gmail.com>
2020-08-23 14:52:06 -0400

    Update users.py

--- users.py
+++ users.py
@@ -6,8 +6,11 @@ class newUser():
         sqliteConnection = sqlite3.connect(database)
         cursor = sqliteConnection.cursor()
 
-        cursor.execute("INSERT INTO user (userID, userName, password) VALUES (1, userName, password)")
+        cursor.execute("INSERT INTO user(userID, userName, password) VALUES (1, userName, password);")
         print("Successfully added new user:", userName)
+        
+        #cursor.execute("SELECT * FROM user;")
+        #print(cursor.fetchall())
 
         sqliteConnection.commit()
         cursor.close()
